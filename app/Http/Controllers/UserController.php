1606f2b3e9ea8271c86104b81548aec904dddbcb
Denis18UKS <honorxpremium75@gmail.com>
2024-12-17 12:45:21 +0500

    operaBash-routes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use App\Models\Age;
 use App\Models\News;
+use App\Models\Event;
 
 
 class UserController extends Controller
@@ -49,5 +50,159 @@ class UserController extends Controller
         } else {
             return back()->withErrors(['email' => 'Неверные данные!'])->withInput();
         }
+    }
+
+
+    public function adminIndex()
+    {
+        $news = DB::table('news')->get();
+        return view('admin/adminInex', compact('news'));
+    }
+
+
+    public function newsShow($id)
+    {
+        $news = News::findOrFail($id);
+        return view('newsShow', compact('news'));
+    }
+
+    public function eventShow($id)
+    {
+        $events = DB::table('events')
+            ->join('age_restrictions', 'events.id_age_restriction', '=', 'age_restrictions.id')
+            ->select('events.title', 'events.subtitle', 'events.duration', 'age_restrictions.tvalue', 'events.description', 'events.show_date')
+            ->where('events.id', '=', $id)
+            ->first();
+
+        return view('eventShow', compact('events'));
+    }
+
+    public function addNewsForm()
+    {
+        return view('admin.addNews');
+    }
+
+    public function addNews(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required|string|max:255',
+            'image' => 'required|string',
+        ]);
+
+        News::create([
+            'title' => $request->input('title'),
+            'description' => $request->input('description'),
+            'image' => $request->input('image'),
+        ]);
+
+        return redirect()->route('adminIndex')->with('success', 'Новость успешно добавлена!');
+
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect()->route('index')->with('success', 'Вы успешно вышли');
+    }
+
+    public function editNewsForm($id)
+    {
+        $news = News::findOrFail($id);
+        return view('admin.editNews', compact('news'));
+    }
+
+    public function editNews(Request $request, $id)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required|string|max:255',
+            'image' => 'required|string',
+        ]);
+
+        $news = News::findOrFail($id);
+
+        $news->update([
+            'title' => $request->input('title'),
+            'description' => $request->input('description'),
+            'image' => $request->input('image'),
+        ]);
+
+        return redirect()->route('adminIndex')->with('success', 'Новость успешно обновлена!');
+    }
+
+    public function admin_poster_add(Request $request)
+    {
+        $events = Event::create([
+            'title' => $request->title,
+            'subtitle' => $request->subtitle,
+            'duration' => $request->duration,
+            'id_age_restriction' => $request->id_age_restriction,
+            'description' => $request->description,
+            'team' => $request->team,
+            'image' => $request->image,
+            'show_date' => $request->show_date,
+        ]);
+
+        return redirect()->route('adminEvent');
+    }
+
+    public function admin_poster_edit_post(Request $request, Event $id)
+    {
+        $id = $request->id;
+        $events = Event::where('id', $id)->update([
+            'title' => $request->title,
+            'subtitle' => $request->subtitle,
+            'duration' => $request->duration,
+            'id_age_restriction' => $request->id_age_restriction,
+            'description' => $request->description,
+            'team' => $request->team,
+            'image' => $request->image,
+            'show_date' => $request->show_date,
+        ]);
+
+        return redirect()->route('adminEvent');
+    }
+
+    public function editEventForm($id)
+    {
+        $event = Event::findOrFail($id);
+        $ageRestrictions = Age::all();
+
+        return view('admin/editEvent', compact('event', 'ageRestrictions'));
+    }
+
+    public function editEvent(Request $request, $id)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'subtitle' => 'required|string',
+            'id_age_restriction' => 'required|integer',
+            'duration' => 'required|string',
+            'show_date' => 'required|date',
+            'description' => 'required|string',
+            'team' => 'required|string',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif',
+        ]);
+
+        $event = Event::findOrFail($id);
+
+        $event->update([
+            'title' => $request->input('title'),
+            'subtitle' => $request->input('subtitle'),
+            'id_age_restriction' => $request->input('id_age_restriction'),
+            'duration' => $request->input('duration'),
+            'show_date' => $request->input('show_date'),
+            'description' => $request->input('description'),
+            'team' => $request->input('team'),
+        ]);
+
+        if ($request->hasFile('image')) {
+            $imagePath = $request->file('image')->store('event_images', 'public');
+            $event->image = $imagePath;
+            $event->save();
+        }
+
+        return redirect()->route('adminIndex')->with('success', 'Новость успешно обновлена!');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [UserController::class, 'index'])->name('index');
+Route::get('/allEvents', [UserController::class, 'allEvents'])->name('allEvents');
+Route::get('/allNews', [UserController::class, 'allNews'])->name('allNews');
+Route::get('/newsShow/{id}', [UserController::class, 'newsShow'])->name('newsShow');
+Route::get('/eventShow/{id}', [UserController::class, 'eventShow'])->name('eventShow');
+
+Route::get('/logout', [UserController::class, 'logout'])->name('logout');
+
+Route::get('/signin', [UserController::class, 'signin_show'])->name('signin_show');
+
+Route::post('/signin', [UserController::class, 'signin'])->name('signin');
+Route::post('/addNews', [UserController::class, 'addNews'])->name('addNews');
+
+Route::get('/admin/adminIndex', [UserController::class, 'adminIndex'])->name('adminIndex');
+Route::get('/admin/adminEvent', [UserController::class, 'adminEvent'])->name('adminEvent');
+Route::get('/edit-news/{id}', [UserController::class, 'editNewsForm'])->name('editNewsForm');
+
+Route::post('/edit-news/{id}', [UserController::class, 'editNews'])->name('editNews');
+Route::post('/admin_poster_add', [UserController::class, 'admin_poster_add'])->name('admin_poster_add');
+Route::post('/admin_poster_edit_post', [UserController::class, 'admin_poster_edit_post'])->name('admin_poster_edit_post');
+
+Route::get('/edit-event/{id}', [UserController::class, 'editEventForm'])->name('editEventForm');
+Route::post('/edit-event/{id}', [UserController::class, 'editEvent'])->name('editEvent');
+
+
+
